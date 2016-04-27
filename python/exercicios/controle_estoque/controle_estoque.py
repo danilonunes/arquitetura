ad934535ea2566546879a00751040a2354c2aacd
danilonunes <danilo.nunes@gmail.com>
2016-04-27 01:22:40 -0300

    add métodos para manipulação de objetos Categoria

--- python/exercicios/controle_estoque/controle_estoque.py
+++ python/exercicios/controle_estoque/controle_estoque.py
@@ -21,6 +21,9 @@ class ControleEstoqueController():
         self.__next_cat_prod_id = 1 # próximo ID para Categoria de Produto
         self.__next_prod_id = 1 # próximo ID para Produto
 
+    def __incrementaProxIdCategoriaProduto(self):
+        self.__next_cat_prod_id = self.__next_cat_prod_id + 1
+        
     # métodos para manipulação de objetos UnidadeMedida
     def incluirUnidadeMedida(self, pNome, pSigla):
         if pSigla in self.__unidades_medida:
@@ -57,20 +60,51 @@ class ControleEstoqueController():
 
         t = [] # instanciando uma lista
         for i in self.__unidades_medida.items():
-            t.append((i[0], i[1].nome)) # adiciona uma tupla dentro da lista l
+            t.append((i[0], i[1].nome)) # adiciona uma tupla dentro da lista t
         return t
 
     # fim *** métodos para manipulação de objetos UnidadeMedida
 
     # métodos para manipulação de objetos CategoriaProduto
-    def incluirCategoriaProduto(self):
-        pass
+    def incluirCategoriaProduto(self, pNome, pDescricao):
+        cp = CategoriaProduto()
+        cp.nome = pNome
+        cp.descricao = pDescricao
+        self.__categorias_produto[self.__next_cat_prod_id] = cp
+        self.__incrementaProxIdCategoriaProduto()
+        
+    def excluirCategoriaProduto(self, pId):
+        if not pId in self.__categorias_produto:
+            raise Exception('Categoria de produto com ID \'', pId, '\' não encontrada.')
 
-    def excluirCategoriaProduto(self):
-        pass
+        del self.__categorias_produto[pId]
+        
 
-    def alterarCategoriaProduto(self):
-        pass
+    def alterarCategoriaProduto(self, pId, pNome, pDescricao):
+        if pId in self.__categorias_produto:
+            if (not pNome is None) or (self.__categorias_produto[pId].nome != pNome):
+                self.__categorias_produto[pId].nome = pNome
+            if (not pDescricao is None) or (self.__categorias_produto[pId].descricao != pDescricao):
+                self.__categorias_produto[pId].descricao = pDescricao
+        else:
+            raise Exception('Categoria de produto com ID \'', pId, '\' não encontrada.')
+    
+    
+    def getListaCategoriaProduto(self):
+        '''
+            Método que retorna uma lista onde cada item representa um objeto
+            CategoriaProduto em forma de tupla.
+                Ex.:
+                        cps = getListaCategoriaProduto()
+                        if not len(cps) == 0: # verifica se não é vazio
+                            print('ID: ', cps[0][0], ' Nome: ', cps[0][1], ' Descrição: ', cps[0][2])
+        '''
+
+        t = [] # instanciando uma lista
+        for i in self.__categorias_produto.items():
+            t.append((i[0], i[1].nome, i[1].descricao)) # adiciona uma tupla dentro da lista t
+        return t
+
     # fim *** métodos para manipulação de objetos CategoriaProduto
 
     # métodos para manipulação de objetos Produto
@@ -122,7 +156,7 @@ class CategoriaProduto:
 
     @nome.setter
     def nome(self, value):
-        self.__nome = srt(value)
+        self.__nome = str(value)
 
     @property
     def descricao(self):
@@ -130,7 +164,7 @@ class CategoriaProduto:
 
     @descricao.setter
     def descricao(self, value):
-        self.__descricao = srt(value)
+        self.__descricao = str(value)
 
 class Produto:
     def __init__(self):
@@ -210,14 +244,38 @@ class Produto:
         self.__vr_max_venda = value if value > 0 else 0
 
 if __name__ == '__main__':
+    l_tam_menu = 100
+    l_menu = '-' * l_tam_menu
+    l_menu_um = 'Unidades de Medida'
+    l_menu_um_len = len(l_menu_um)
+    l_menu_cp = 'Categoria de Produto'
+    l_menu_cp_len = len(l_menu_cp)
+    
     ce = ControleEstoqueController()
 
     ce.incluirUnidadeMedida('quilo', 'kg')
     ce.incluirUnidadeMedida('peça', 'pc')
     ce.incluirUnidadeMedida('metro', 'm')
     ce.incluirUnidadeMedida('mililitros', 'ml')
+
+    print(l_menu)
+    print('-' * int((l_tam_menu - l_menu_um_len) / 2), ' ' * 5, l_menu_um, ' ' * 5, '-' * int((l_tam_menu - l_menu_um_len) / 2))
+    print(l_menu)
     for i in sorted(ce.getListaUnidadeMedida()):
         print(i[0], ': ', i[1])
+    print(l_menu)
+
+    ce.incluirCategoriaProduto('Produtos relacionados a limpeza', 'Limpeza')
+    ce.incluirCategoriaProduto('Produtos relacionados a alimentação humana', 'Alimentação humana')
+    ce.incluirCategoriaProduto('Produtos relacionados a bebida alcoólica', 'Bebida alcoólica')
+    ce.incluirCategoriaProduto('Produtos relacionados a alimentação de pets', 'Alimentação pets')
+
+    print(l_menu)
+    print('-' * int((l_tam_menu - l_menu_cp_len) / 2), ' ' * 5, l_menu_cp, ' ' * 5, '-' * int((l_tam_menu - l_menu_cp_len) / 2))
+    print(l_menu)
+    for i in sorted(ce.getListaCategoriaProduto()):
+        print(i[0], '-> ', i[1], ' -> ', i[2])
+    print(l_menu)
 
 
 #    while true:
