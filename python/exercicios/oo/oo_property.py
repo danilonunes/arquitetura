64e0d981aa4e38458ebdda14795d2057b8a22a70
Danilo Nunes <danilo.nunes@gmail.com>
2016-08-30 16:36:52 -0300

    Exercícios de revisão de OO

---
+++ python/exercicios/oo/oo_property.py
@@ -0,0 +1,23 @@
+#!/usr/bin/env python3
+
+# ************************************************
+#              IFNMG - Campus Januária
+#                       TADS
+#              Arquitetura de Software
+# ************************************************
+# ***************** exercício 01 ***************** 
+# ************************************************
+# Faça um programa em Python (>= 3) onde exista
+# uma classe Pessoa com os seguintes atributos:
+#   - nome;
+#   - sexo;
+#   - data_nasc (import datetime).
+# 
+# Os dados para referentes aos atributos devem
+# ser informados no momento da instancialização
+# do objeto. Cada atributo deve ser representado
+# por uma propriedade (RW).
+#
+# ***************** ************ ***************** 
+
+
