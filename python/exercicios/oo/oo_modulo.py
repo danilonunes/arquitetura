ec36d4e68dbefa28ace193cc504a71ae04e53d44
Danilo Nunes <danilo.nunes@gmail.com>
2016-09-20 18:03:59 -0300

    Ajuste para uso de property no __init__

--- python/exercicios/oo/oo_modulo.py
+++ python/exercicios/oo/oo_modulo.py
@@ -10,9 +10,9 @@
 #                       TADS
 #              Arquitetura de Software
 # ************************************************
-# 
+#
 # -------------------- constantes -------------------
-# as constantes estão definidas em um módulo 
+# as constantes estão definidas em um módulo
 # próprio chamado de constantes.py
 
 import datetime
@@ -24,67 +24,67 @@ class Pessoa(object):
         Classe Pessoa para um simples cadastro
     '''
     def __init__(self, nome, sexo, dt_nasc, est_civil):
-        self.__nome = nome
-        self.__sexo = sexo
-        self.__data_nasc = dt_nasc
-        self.__est_civil = est_civil
-    
+        self.nome = nome
+        self.sexo = sexo
+        self.data_nasc = dt_nasc
+        self.estadoCivil = est_civil
+
     @property
     def nome(self):
         return self.__nome
-    
+
     @nome.setter
     def nome(self, valor):
         if not valor or (valor == ''):
             raise Exception("O valor informado para o atributo nome é inválido.")
-        self.__nome = valor         
+        self.__nome = valor
 
     @property
     def sexo(self):
         return self.__sexo
-    
+
     @sexo.setter
     def sexo(self, valor):
-        if (not valor or (valor == '') 
-            or (valor not in ['F', 
+        if (not valor or (valor == '')
+            or (valor not in ['F',
                 'f', 'M', 'm', 'O', 'o'])):
             raise Exception("O valor informado para o atributo sexo é inválido.")
-        self.__sexo = valor         
-        
+        self.__sexo = valor
+
     @property
     def data_nasc(self):
         return self.__data_nasc
-    
+
     @data_nasc.setter
     def data_nasc(self, valor):
         if not valor or not isinstance(valor, datetime.date):
             raise Exception("O valor informado para o atributo data_nasc é inválido.")
-        
+
         #verificando se a data informada é maior que a data atual
         if not (valor <= datetime.date.today()):
             m = "Uma data de nascimento futura não é aceitável.\nValor informado: {0}".format(valor.isoformat())
             raise Exception(m)
-            
+
         self.__data_nasc = valor
 
-    
+
     @property
     def estadoCivil(self):
         return self.__est_civil
-    
+
     @estadoCivil.setter
     def estadoCivil(self, valor):
         if not valor or (valor not in ESTADO_CIVIL.keys()):
             raise Exception("O valor informado para o atributo estadoCivil é inválido.")
-        self.__est_civil = valor         
+        self.__est_civil = valor
 
 
 
 # !!!!!!!!!!!!!!!! Testando... !!!!!!!!!!!!!!!!!!!
 if __name__ == '__main__':
 
-    p1 = Pessoa('Danilo Nunes', 
-                MASCULINO, 
+    p1 = Pessoa('Danilo Nunes',
+                MASCULINO,
                 datetime.date(1983, 3, 20),
                 CASADO)
 
@@ -93,4 +93,3 @@ if __name__ == '__main__':
     print('Modelo de objetos')
     print(p1.nome, '\n', p1.sexo, '\n', p1.data_nasc, '\n',
         p1.estadoCivil)
-
