1f98919123802793068f5275d3304d0a9c601098
Danilo Nunes <danilo.nunes@gmail.com>
2017-06-28 18:50:03 -0300

    Atualização do model

--- python/exemplos/xmlrpc/bd_in_memory/model.py
+++ python/exemplos/xmlrpc/bd_in_memory/model.py
@@ -24,11 +24,41 @@ class ItemVenda(object):
 class Venda(object):
     def __init__(self):
         self.itens = {}
+        self.desconto = 0.00
 
     def valorTotal(self):
         vt = 0.00
-        
+
         for i in self.itens:
             vt =+ i.subTotal
 
+        vt =- self.desconto
+
         return vt
+
+    def addItem(self, pProduto, pQtde=1):
+        iv = ItemVenda()
+        iv.setProduto(pProduto, pQtde)
+        self.itens[pProduto.id] = iv
+
+    def delItem(self, pIdProduto, pQtde=1):
+        if not pIdProduto in self.itens:
+            raise Exception('Produto não identificado na venda.')
+
+        if self.itens[pIdProduto].quantidade < pQtde:
+            raise Exception('A quantidade a ser removida é maior que a existente na venda.')
+
+        elif self.itens[pIdProduto].quantidade = pQtde:
+            del self.itens[pIdProduto]
+
+        else:
+            self.itens[pIdProduto].quantidade =- pQtde
+
+    def getItens(self):
+        li = ()
+
+        for i in self.itens:
+            li.add(i.produto.id, i.produto.valor_venda, i.quantidade)
+
+        return li
+
